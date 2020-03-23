31b609598beba0254f79ed1af4858cff5b589d95
HDS <jonathan.canales.hds@gmail.com>
2020-03-22 23:02:51 -0500

    transiciones y animaciones

--- app/src/main/java/com/example/proyectokotlin/AdapterLoading.kt
+++ app/src/main/java/com/example/proyectokotlin/AdapterLoading.kt
@@ -1,7 +1,11 @@
 package com.example.proyectokotlin
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_loading.view.*
 
@@ -25,6 +29,21 @@ class AdapterLoading (val data: List<ItemLoading>): RecyclerView.Adapter<Adapter
                 itemView.txtTitle.text = title
                 itemView.txtDesc.text = desc
                 itemView.txtPrice.text = String.format("%.2f",price)
+
+                itemView.setOnClickListener{
+                    val intent = Intent(itemView.context, DetailActivity::class.java)
+                    intent.putExtra("title", title)
+                    intent.putExtra("desc", desc)
+                    intent.putExtra("price", price)
+
+                    val p1: Pair<View, String> = Pair.create(itemView.imgHeader,"transitionHeader")
+                    val p2: Pair<View, String> = Pair.create(itemView.txtTitle,"transitionTitle")
+                    val p3: Pair<View, String> = Pair.create(itemView.txtDesc,"transitionDesc")
+                    val p4: Pair<View, String> = Pair.create(itemView.txtPrice,"transitionPrice")
+
+                    val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity,p1,p2,p3,p4)
+                    itemView.context.startActivity(intent,options.toBundle())
+                }
             }
         }
     }
--- app/src/main/java/com/example/proyectokotlin/DetailActivity.kt
+++ app/src/main/java/com/example/proyectokotlin/DetailActivity.kt
@@ -2,11 +2,22 @@ package com.example.proyectokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_descr.*
+import kotlinx.android.synthetic.main.item_loading.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        intent?.extras?.let {
+            val t1 = intent.extras!!["title"]
+            tvDetailTitulo.text = t1.toString()
+            val t2 = intent.extras!!["desc"]
+            tvDetailDesc.text = t2.toString()
+            val t3 = intent.extras!!["price"]
+            tvDetailPrice.text =  "$ $t3"
+        }
     }
 }
